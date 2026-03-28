42492053bb6a5ac70d62da6333feed9bbe3515f4
Yash Patil <patilyash1502@gmail.com>
2026-03-28 23:55:53 +0530

    Update Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,17 @@ const deleteProduct = async (req, res) => {
   }
 };
 
-export { createProduct, deleteProduct };
+// Update a product
+const updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json({ message: "Product updated successfully", product });
+    } catch (error) {
+        res.status(500).json({ message: "Error updating product", error });
+    }
+};
+
+export { createProduct, deleteProduct, updateProduct};
--- backend/routes/productroutes.js
+++ backend/routes/productroutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createProduct, deleteProduct} from '../controllers/productController.js';
+import { createProduct, deleteProduct, updateProduct} from '../controllers/productController.js';
 import verifyToken from '../middleware/authmiddleware.js';
 import authorizeRoles from '../middleware/rolemiddleware.js';
 
@@ -7,5 +7,7 @@ const router = express.Router();
 
 router.post('/add', verifyToken, authorizeRoles('admin'), createProduct);
 router.delete('/delete/:id', verifyToken, authorizeRoles('admin'), deleteProduct);
+router.put('/update/:id', verifyToken, authorizeRoles('admin'), updateProduct);
 
 export default router;
+
